90438c3aa41b8c7e497eea3c751cffcc01c2cc3b
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-09-13 17:07:01 -0400

    Edit base inquirer

--- app.js
+++ app.js
@@ -1,156 +1,45 @@
 const inquirer = require('inquirer');
-// const generatePage = require('./src/page-template');
-// const { writeFile, copyFile } = require('./utils/generate-site');
 
-const promptUser = () => {
-    console.log(
-        "╔══════════════════╗\n"+
-        "  Employer Manager\n"+
-        "╚══════════════════╝\n");
-
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username (Required)',
-            validate: gitHubNameInput => {
-                if (gitHubNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your Github Username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAbout',
-            message: 'Would you like to enter some information about yourself for an "About" section?(y/N)',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'about',
-            message: 'Provide some information about yourself:',
-            when: ({ confirmAbout }) => {
-                if (confirmAbout) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-    ]);
-};
-
-const promptProject = portfolioData => {
-
-    console.log(`
-  =================
-  Add a New Project
-  =================
-  `);
-    // If there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
+const promptAction = (teamData = []) => {
+    if (!teamData.action) {
+        console.log(
+            "╔══════════════════╗\n" +
+            "  Employer Manager\n" +
+            "╚══════════════════╝\n");
     }
-    return inquirer.prompt([
+    return inquirer.prompt(
         {
-            type: 'input',
-            name: 'name',
-            message: 'What is the name of your project?',
-            validate: projectNameInput => {
-                if (projectNameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your project name!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of the project (Required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your description!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'checkbox',
-            name: 'languages',
-            message: 'What did you build this project with? (Check all that apply)',
-            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
-        },
-        {
-            type: 'input',
-            name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)',
-            validate: githubLinkInput => {
-                if (githubLinkInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your github Link!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'feature',
-            message: 'Would you like to feature this project?',
-            default: false
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
-            default: false
+            type: 'list',
+            name: 'actionInquirer',
+            message: 'What would you like to do?',
+            choices: ['View All Employees', 'Add Employee',
+                'Update Employee Role', 'View All Roles', 'Add Role',
+                'View All Departments', 'Add Department']
         }
-    ]).then(projectData => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-            return promptProject(portfolioData);
-        } else {
-            console.log(portfolioData);
-            return portfolioData;
-        }
-    });
+    )
+        .then(action => {
+            console.log(action.actionInquirer);
+            if (action.actionInquirer === 'View All Employees') {
+                console.log("View Em");
+            } else if (action.actionInquirer === 'Add Employee') {
+                console.log("Add Em");
+            } else if (action.actionInquirer === 'Update Employee Role') {
+                console.log("Update Em Role");
+            } else if (action.actionInquirer === 'View All Roles') {
+                console.log("View Roles");
+            } else if (action.actionInquirer === 'Add Role') {
+                console.log("Add Ro");
+            } else if (action.actionInquirer === 'View All Departments') {
+                console.log("View All De");
+            }
+            else if (action.actionInquirer === 'Add Department') {
+                console.log("Add De");
+            }
+            teamData.action = action.actionInquirer;
+            return promptAction(teamData);
+
+        });
 };
 
-promptUser()
-    .then(promptProject);
-    // .then(portfolioData => {
-    //     return generatePage(portfolioData);
-    // })
-    // .then(pageHTML => {
-    //     return writeFile(pageHTML);
-    // })
-    // .then(writeFileResponse => {
-    //     console.log(writeFileResponse);
-    //     return copyFile();
-    // })
-    // .then(copyFileResponse => {
-    //     console.log(copyFileResponse);
-    // })
-    // .catch(err => {
-    //     console.log(err);
-    // });
+
+promptAction();
