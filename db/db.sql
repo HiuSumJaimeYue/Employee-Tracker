4c8e89192495868ef7f5c66b19b101bf81830c35
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-09-25 14:01:11 -0400

    Setup database

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS employee_data;
+
+CREATE DATABASE employee_data;
+USE employee_data;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+('Rob', 'Smith', 1, 2),
+('Jason', 'Woolf', 2, 2),
+  ('Chole', 'Gaveston',2, 3),
+  ('Charles', 'LeRoi', 1,5),
+  ('Kat', 'Mansfield', 1,3);
